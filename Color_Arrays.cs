99476c786f7c3fd0323d2c942fefe27e077f96bd
Patrick <p77wood@gmail.com>
2024-10-24 22:14:29 -0500

    Initial TODOs

---
+++ Color_Arrays.cs
@@ -0,0 +1,48 @@
+public static class Color_Arrays{
+
+    /*
+    g = green
+    y = yellow
+    p = pink
+    o = orange
+    c = cyan
+    p = pink
+    m = magenta (dark purple)
+    r = red
+    b = blue
+    w = white
+    | = thin white vertical bar separating the two colors
+
+    periods are grey octagons
+    all other punctuation = blank space
+    */
+
+    public static Char[] A = ['g', ' ' ,'g'];
+    public static Char[] B = ['y', ' ' ,'y'];
+    public static Char[] C = ['p', ' ' ,'p'];
+    public static Char[] D = ['o', ' ' ,'o'];
+    public static Char[] E = ['c', ' ' ,'c'];
+    public static Char[] F = ['p', ' ' ,'p'];
+    public static Char[] G = ['m', ' ' ,'m'];
+    public static Char[] H = ['r', ' ' ,'r'];
+    public static Char[] I = ['b', ' ' ,'b'];
+    public static Char[] J = ['g', '|' ,'w'];
+    public static Char[] K = ['g', '|' ,'g'];
+    public static Char[] L = ['g', '|' ,'y'];
+    public static Char[] M = ['g', '|' ,'p'];
+    public static Char[] N = ['g', '|' ,'o'];
+    public static Char[] O = ['g', '|' ,'c'];
+    public static Char[] P = ['g', '|' ,'p'];
+    public static Char[] Q = ['g', '|' ,'m'];
+    public static Char[] R = ['g', '|' ,'r'];
+    public static Char[] S = ['g', '|' ,'b'];
+    public static Char[] T = ['y', '|' ,'w'];
+    public static Char[] U = ['y', '|' ,'g'];
+    public static Char[] V = ['y', '|' ,'y'];
+    public static Char[] W = ['y', '|' ,'p'];
+    public static Char[] X = ['y', '|' ,'o'];
+    public static Char[] Y = ['y', '|' ,'c'];
+    public static Char[] Z = ['y', '|' ,'p'];
+
+
+} 
---
+++ Flaxseed.cs
@@ -0,0 +1,12 @@
+﻿class Flaxseed
+{
+    static void Main(string[] args)
+    {
+        // TODO: convert text to color blocks like in New Order - Blue Monday (Official Lyric Video)
+        // TODO: get input
+        // TODO: break input into individual words
+        // TODO: break individual words into character arrays
+        // TODO: analyze character arrays, construct color array (as text, e.x. [['r', 'g'], ['b', 'r']])
+        // TODO: somehow generate an actual image using the color arrays.
+    }
+}
