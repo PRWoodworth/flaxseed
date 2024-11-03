5efb8d07cd44cb55713b898c6bda954268a1368d
Patrick <p77wood@gmail.com>
2024-11-02 19:15:31 -0500

    cleanup

--- Flaxseed.cs
+++ Flaxseed.cs
@@ -1,38 +1,38 @@
-﻿using Microsoft.VisualBasic;
+﻿// https://docs.sixlabors.com/articles/imagesharp/gettingstarted.html
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Drawing;
 using SixLabors.ImageSharp.Drawing.Processing;
 using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Processing;
-using SixLabors.ImageSharp.Processing.Processors.Drawing;
 
-namespace flaxseed {
-    
-    
-    
+namespace flaxseed
+{
+
+
+
     class Flaxseed{
 
         static void Main(string[] args){
             var color_codex = new Color_Arrays();
-            List<List<List<String>>> colorized_input = Colorize_Text("test input to make sure everything is working as intended to avoid sudden problems", color_codex);
+            List<List<List<string>>> colorized_input = Colorize_Text("test input to make sure everything is working as intended to avoid sudden problems", color_codex);
             Generate_Image(colorized_input, color_codex);
             // TODO: convert text to color blocks like in New Order - Blue Monday (Official Lyric Video)
 
         }
 
-        static List<List<List<String>>> Colorize_Text(String input, Color_Arrays color_codex){
+        static List<List<List<string>>> Colorize_Text(string input, Color_Arrays color_codex){
             // TODO: console prompt to get input from user 
             // TODO: make actual window to get input from user
             
-            Dictionary<Char, List<String>> Letter_Colors = color_codex.Init_Letter_Colors_Dict();
-            String[] split_input = input.Split(' ');
+            Dictionary<char, List<string>> Letter_Colors = color_codex.Init_Letter_Colors_Dict();
+            string[] split_input = input.Split(' ');
 
             // TODO: refactor to leave whitespace intact
-            List<List<List<String>>> input_colorization = [];
+            List<List<List<string>>> input_colorization = [];
 
 
             foreach (var word in split_input){
-                List<List<String>> word_colorization = [];
+                List<List<string>> word_colorization = [];
                 foreach (var letter in word){
                     word_colorization.Add(Letter_Colors[char.ToUpper(letter)]);
                 }
@@ -42,7 +42,7 @@ namespace flaxseed {
             return input_colorization;
         }
 
-        static void Generate_Image(List<List<List<String>>> colorized_input, Color_Arrays color_codex){
+        static void Generate_Image(List<List<List<string>>> colorized_input, Color_Arrays color_codex){
             int width = 1900;
             int height = 1200;
             Dictionary<String, SixLabors.ImageSharp.Color> color_dict = color_codex.Init_Color_Codes_Dict();
@@ -55,10 +55,10 @@ namespace flaxseed {
 
                         
             // TODO: prompt for user to specify file save location, as part of an actual app or something idk
-            image.Save("test.jpg");
+            image.Save("test.png");
         }
 
-        static Image<Rgba32> Generate_Rectangle_Codes_For_Word(Image<Rgba32> canvas, List<List<String>> word, Dictionary<String, SixLabors.ImageSharp.Color> color_dict, int word_number){
+        static Image<Rgba32> Generate_Rectangle_Codes_For_Word(Image<Rgba32> canvas, List<List<string>> word, Dictionary<string, SixLabors.ImageSharp.Color> color_dict, int word_number){
             int letter_number = 0;
             foreach (var letter in word){
                 canvas = Generate_Rectangle_Code_For_Letter(canvas, letter, color_dict, word_number, letter_number);
@@ -67,7 +67,7 @@ namespace flaxseed {
             return canvas;
         }
 
-        static Image<Rgba32> Generate_Rectangle_Code_For_Letter(Image<Rgba32> canvas, List<String> letter, Dictionary<String, SixLabors.ImageSharp.Color> color_dict, int word_number, int letter_number){
+        static Image<Rgba32> Generate_Rectangle_Code_For_Letter(Image<Rgba32> canvas, List<string> letter, Dictionary<string, SixLabors.ImageSharp.Color> color_dict, int word_number, int letter_number){
             float y = 60 * word_number;
             float starting_x = 40 * letter_number;
             switch (letter[0]){
@@ -76,7 +76,6 @@ namespace flaxseed {
                     canvas.Mutate(x => x.Fill(color_dict[letter[1]], new RectangularPolygon(starting_x, y, 20, 60)));
                     canvas.Mutate(x => x.Fill(color_dict[letter[3]], new RectangularPolygon(starting_x + 20, y, 20, 60)));
                     if(letter[2].Equals("||")){
-                        // TODO: get the bar placed properly
                         canvas.Mutate(x => x.Fill(SixLabors.ImageSharp.Color.Grey, new RectangularPolygon(starting_x + 18, y, 5, 60)));
                     }
                     break;
