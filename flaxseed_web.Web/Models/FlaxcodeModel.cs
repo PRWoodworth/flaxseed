a4980572c1691be74af8ae45cbafdcbd3085c1ea
Patrick Woodworth <p77wood@gmail.com>
2025-04-01 20:08:40 -0500

    need to deal with possible null values in places

--- flaxseed_web.Web/Models/FlaxcodeModel.cs
+++ flaxseed_web.Web/Models/FlaxcodeModel.cs
@@ -3,8 +3,8 @@ using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp;
 public class FlaxcodeModel
 {
-    public string FlaxcodeInput { get; set; }
-    public Image<Rgba32> FlaxcodeOutput { get; set; }
+    public string ? FlaxcodeInput { get; set; }
+    public Image<Rgba32> ? FlaxcodeOutput { get; set; }
     public FlaxcodeModel(string input) { FlaxcodeInput = input; }
     public FlaxcodeModel( ) { }
 }
