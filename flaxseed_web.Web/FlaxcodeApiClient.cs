eb263bbeba0913a0acb5ce86b3b1192f2c6c02b3
Patrick Woodworth <p77wood@gmail.com>
2025-04-02 13:49:52 -0500

    added important todo so i remember to fix a problem

--- flaxseed_web.Web/FlaxcodeApiClient.cs
+++ flaxseed_web.Web/FlaxcodeApiClient.cs
@@ -10,10 +10,18 @@ public class FlaxcodeApiClient(HttpClient httpClient)
         try
         {
             //TODO: this cannot be done as JSON as-is. Need to refactor sending end to provide compatible data or find a work-around on receiving end. 
-            String generated_flaxcode = await httpClient.GetStringAsync("/getflaxcode", cancellationToken: cancellationToken);
+            String generated_flaxcode = await httpClient.GetStringAsync("/getflaxcode", cancellationToken);
+        //Error preventing progress is exclusively on above line
+        //TODO: this isn't actually sending a request body i think. i should probably fix that. 
+            /*
+             * System.InvalidOperationException: 
+             * The type 'System.ReadOnlySpan`1[System.Byte]' of property 'Preamble' on type 'System.Text.Encoding' is invalid for serialization or deserialization 
+             * because it is a pointer type, is a ref struct or contains generic parameters that have not been replaced by specific types.
+             * */
+
             byte[] bytes = Convert.FromBase64String(generated_flaxcode);
             Image<Rgba32> image;
-            using (MemoryStream ms = new MemoryStream(bytes))
+            using (MemoryStream ms = new(bytes))
             {
                 image = (Image<Rgba32>)Image.Load(ms);
             }
